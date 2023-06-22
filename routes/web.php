dc545e8be6727907963d73d9e60fe07e20499593
ananda <qq.kurniadi445@gmail.com>
2023-06-22 21:26:22 +0700

    Tambah rute autentikasi (masuk)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AutentikasiController;
+use App\Http\Controllers\DasborController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('autentikasi')->group(function () {
+    Route::name('autentikasi.')->group(function () {
+        Route::get('/masuk', [AutentikasiController::class, 'masuk'])->name('masuk');
+        Route::post('/masuk', [AutentikasiController::class, 'prosesMasuk'])->name('proses-masuk');
+        Route::get('/keluar', [AutentikasiController::class, 'keluar'])->name('keluar');
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [DasborController::class, 'indeks'])->name('dasbor');
 });
