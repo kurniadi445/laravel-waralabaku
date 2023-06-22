815baed505ac00a681ec73bc63f9cdb34cd4d6af
ananda <qq.kurniadi445@gmail.com>
2023-06-22 23:13:11 +0700

    Tambah controller transaksi penjualan (ekspor)

--- app/Http/Controllers/Laporan/TransaksiPenjualanController.php
+++ app/Http/Controllers/Laporan/TransaksiPenjualanController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Laporan;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Laporan\TransaksiPenjualanRepository;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TransaksiPenjualanController extends Controller
 {
@@ -28,5 +30,19 @@ class TransaksiPenjualanController extends Controller
         return view('tampilan.laporan.transaksi-penjualan.indeks', [
             'transaksi' => $transaksi
         ]);
+    }
+
+    public function ekspor(Request $request): Response
+    {
+        $tanggalMulai = $request->query('tanggal-mulai');
+        $tanggalAkhir = $request->query('tanggal-akhir');
+
+        $transaksi = $this->transaksiPenjualanRepository->cariSemua($tanggalMulai, $tanggalAkhir);
+
+        $pdf = Pdf::loadView('tampilan.laporan.transaksi-penjualan.ekspor', [
+            'transaksi' => $transaksi
+        ]);
+
+        return $pdf->stream();
     }
 }
