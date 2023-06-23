fb1b278c0cf8e7e897080678f151d4d944852f72
ananda <qq.kurniadi445@gmail.com>
2023-06-24 00:27:12 +0700

    Tambah controller kinerja cabang (indeks)

---
+++ app/Http/Controllers/Laporan/KinerjaCabangController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Laporan;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Laporan\KinerjaCabangRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class KinerjaCabangController extends Controller
+{
+    private KinerjaCabangRepository $kinerjaCabangRepository;
+
+    public function __construct(KinerjaCabangRepository $kinerjaCabangRepository)
+    {
+        $this->kinerjaCabangRepository = $kinerjaCabangRepository;
+    }
+
+    public function indeks(Request $request): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        $cabang = $request->query('cabang');
+
+        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($cabang);
+
+        return view('tampilan.laporan.kinerja-cabang.indeks', [
+            'kinerja_cabang' => $kinerjaCabang
+        ]);
+    }
+}
---
+++ app/Repositories/Laporan/KinerjaCabangRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories\Laporan;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class KinerjaCabangRepository
+{
+    public function cariSemua($cabang = null): Collection
+    {
+        $kinerjaCabang = DB::table('v_transaksi')->selectRaw('json_unquote(json_extract(cabang, \'$.nama_cabang\')) nama_cabang');
+        $kinerjaCabang = $kinerjaCabang->addSelect(DB::raw('format(count(*), 0) jumlah_transaksi'));
+        $kinerjaCabang = $kinerjaCabang->addSelect(DB::raw('concat(\'Rp\', format(sum(total), 0)) as total'));
+
+        if ($cabang) {
+            $kinerjaCabang = $kinerjaCabang->whereRaw('json_unquote(json_extract(cabang, \'$.nama_cabang\')) like ?', [sprintf('%%%s%%', $cabang)]);
+        }
+
+        $kinerjaCabang = $kinerjaCabang->groupByRaw('1');
+
+        $kinerjaCabang = DB::query()->fromSub($kinerjaCabang, 'cte_transaksi');
+        $kinerjaCabang = $kinerjaCabang->selectRaw('row_number() over (order by total desc) no');
+        $kinerjaCabang = $kinerjaCabang->addSelect('cte_transaksi.*');
+
+        return $kinerjaCabang->get();
+    }
+}
