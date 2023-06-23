72757fa5dbb0462c75e1584566fb83b839e55e36
ananda <qq.kurniadi445@gmail.com>
2023-06-24 01:53:35 +0700

    Batasi hak akses admin

--- routes/web.php
+++ routes/web.php
@@ -35,23 +35,25 @@ Route::middleware('auth')->group(function () {
 
     Route::prefix('master')->group(function () {
         Route::name('master.')->group(function () {
-            Route::prefix('cabang')->group(function () {
-                Route::get('/', [CabangController::class, 'indeks'])->name('cabang');
-                Route::get('/tambah', [CabangController::class, 'tambah'])->name('cabang.tambah');
-                Route::post('/tambah', [CabangController::class, 'prosesTambah'])->name('cabang.proses-tambah');
-            });
+            Route::middleware('cek-admin')->group(function () {
+                Route::prefix('cabang')->group(function () {
+                    Route::get('/', [CabangController::class, 'indeks'])->name('cabang');
+                    Route::get('/tambah', [CabangController::class, 'tambah'])->name('cabang.tambah');
+                    Route::post('/tambah', [CabangController::class, 'prosesTambah'])->name('cabang.proses-tambah');
+                });
 
-            Route::prefix('produk')->group(function () {
-                Route::get('/', [ProdukController::class, 'indeks'])->name('produk');
-                Route::get('/tambah', [ProdukController::class, 'tambah'])->name('produk.tambah');
-                Route::post('/tambah', [ProdukController::class, 'prosesTambah'])->name('produk.proses-tambah');
-                Route::get('/edit/{uuid}', [ProdukController::class, 'edit'])->whereUuid('uuid')->name('produk.edit');
-                Route::put('/edit/{uuid}', [ProdukController::class, 'prosesEdit'])->whereUuid('uuid')->name('produk.proses-edit');
-                Route::delete('/hapus/{uuid}', [ProdukController::class, 'hapus'])->whereUuid('uuid')->name('produk.hapus');
-            });
+                Route::prefix('produk')->group(function () {
+                    Route::get('/', [ProdukController::class, 'indeks'])->name('produk');
+                    Route::get('/tambah', [ProdukController::class, 'tambah'])->name('produk.tambah');
+                    Route::post('/tambah', [ProdukController::class, 'prosesTambah'])->name('produk.proses-tambah');
+                    Route::get('/edit/{uuid}', [ProdukController::class, 'edit'])->whereUuid('uuid')->name('produk.edit');
+                    Route::put('/edit/{uuid}', [ProdukController::class, 'prosesEdit'])->whereUuid('uuid')->name('produk.proses-edit');
+                    Route::delete('/hapus/{uuid}', [ProdukController::class, 'hapus'])->whereUuid('uuid')->name('produk.hapus');
+                });
 
-            Route::prefix('pengguna')->group(function () {
-                Route::get('/', [PenggunaController::class, 'indeks'])->name('pengguna');
+                Route::prefix('pengguna')->group(function () {
+                    Route::get('/', [PenggunaController::class, 'indeks'])->name('pengguna');
+                });
             });
 
             Route::prefix('stok')->group(function () {
@@ -67,9 +69,11 @@ Route::middleware('auth')->group(function () {
                 Route::get('/ekspor', [TransaksiPenjualanController::class, 'ekspor'])->name('transaksi-penjualan.ekspor');
             });
 
-            Route::prefix('kinerja-cabang')->group(function () {
-                Route::get('/', [KinerjaCabangController::class, 'indeks'])->name('kinerja-cabang');
-                Route::get('/ekspor', [KinerjaCabangController::class, 'ekspor'])->name('kinerja-cabang.ekspor');
+            Route::middleware('cek-admin')->group(function () {
+                Route::prefix('kinerja-cabang')->group(function () {
+                    Route::get('/', [KinerjaCabangController::class, 'indeks'])->name('kinerja-cabang');
+                    Route::get('/ekspor', [KinerjaCabangController::class, 'ekspor'])->name('kinerja-cabang.ekspor');
+                });
             });
         });
     });
