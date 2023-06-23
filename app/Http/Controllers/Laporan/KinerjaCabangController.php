de8b3de1e0a49475df0f7763cb596a10dd8f2263
ananda <qq.kurniadi445@gmail.com>
2023-06-24 00:34:51 +0700

    Tambah controller kinerja cabang (ekspor)

--- app/Http/Controllers/Laporan/KinerjaCabangController.php
+++ app/Http/Controllers/Laporan/KinerjaCabangController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Laporan;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Laporan\KinerjaCabangRepository;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class KinerjaCabangController extends Controller
 {
@@ -27,5 +29,18 @@ class KinerjaCabangController extends Controller
         return view('tampilan.laporan.kinerja-cabang.indeks', [
             'kinerja_cabang' => $kinerjaCabang
         ]);
+    }
+
+    public function ekspor(Request $request): Response
+    {
+        $cabang = $request->query('cabang');
+
+        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($cabang);
+
+        $pdf = Pdf::loadView('tampilan.laporan.kinerja-cabang.ekspor', [
+            'kinerja_cabang' => $kinerjaCabang
+        ]);
+
+        return $pdf->stream();
     }
 }
