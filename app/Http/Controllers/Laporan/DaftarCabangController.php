3245b83c7372a794ff3cb42b5f16ffdd1bfd3853
ananda <qq.kurniadi445@gmail.com>
2023-07-06 20:30:32 +0700

    Tambah controller daftar cabang (ekspor)

--- app/Http/Controllers/Laporan/DaftarCabangController.php
+++ app/Http/Controllers/Laporan/DaftarCabangController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Laporan;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Laporan\DaftarCabangRepository;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class DaftarCabangController extends Controller
@@ -27,5 +29,18 @@ class DaftarCabangController extends Controller
         return view('tampilan.laporan.daftar-cabang.indeks', [
             'daftar_cabang' => $daftarCabang
         ]);
+    }
+
+    public function ekspor(): Response
+    {
+        $idPengguna = Auth::user()->getAuthIdentifier();
+
+        $daftarCabang = $this->daftarCabangRepository->cariSemua($idPengguna);
+
+        $pdf = Pdf::loadView('tampilan.laporan.daftar-cabang.ekspor', [
+            'daftar_cabang' => $daftarCabang
+        ]);
+
+        return $pdf->stream();
     }
 }
