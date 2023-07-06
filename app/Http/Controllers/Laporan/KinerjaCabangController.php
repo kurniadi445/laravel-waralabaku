09ca429003d8c1d4fbe66efa5772c698586bef7d
ananda <qq.kurniadi445@gmail.com>
2023-07-07 04:53:25 +0700

    Edit kueri pada kinerja cabang

--- app/Http/Controllers/Laporan/KinerjaCabangController.php
+++ app/Http/Controllers/Laporan/KinerjaCabangController.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class KinerjaCabangController extends Controller
 {
@@ -22,9 +23,14 @@ class KinerjaCabangController extends Controller
 
     public function indeks(Request $request): View|\Illuminate\Foundation\Application|Factory|Application
     {
+        $pengguna = Auth::user();
+
+        $idPengguna = $pengguna->getAuthIdentifier();
+        $level = $pengguna->{'level'};
+
         $cabang = $request->query('cabang');
 
-        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($cabang);
+        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($idPengguna, $level, $cabang);
 
         return view('tampilan.laporan.kinerja-cabang.indeks', [
             'kinerja_cabang' => $kinerjaCabang
@@ -33,9 +39,14 @@ class KinerjaCabangController extends Controller
 
     public function ekspor(Request $request): Response
     {
+        $pengguna = Auth::user();
+
+        $idPengguna = $pengguna->getAuthIdentifier();
+        $level = $pengguna->{'level'};
+
         $cabang = $request->query('cabang');
 
-        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($cabang);
+        $kinerjaCabang = $this->kinerjaCabangRepository->cariSemua($idPengguna, $level, $cabang);
 
         $pdf = Pdf::loadView('tampilan.laporan.kinerja-cabang.ekspor', [
             'kinerja_cabang' => $kinerjaCabang
--- app/Repositories/Laporan/KinerjaCabangRepository.php
+++ app/Repositories/Laporan/KinerjaCabangRepository.php
@@ -2,16 +2,26 @@
 
 namespace App\Repositories\Laporan;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class KinerjaCabangRepository
 {
-    public function cariSemua($cabang = null): Collection
+    public function cariSemua($idPengguna, $level, $cabang = null): Collection
     {
         $kinerjaCabang = DB::table('v_transaksi')->selectRaw('json_unquote(json_extract(cabang, \'$.nama_cabang\')) nama_cabang');
         $kinerjaCabang = $kinerjaCabang->addSelect(DB::raw('format(count(*), 0) jumlah_transaksi'));
         $kinerjaCabang = $kinerjaCabang->addSelect(DB::raw('concat(\'Rp\', format(sum(total), 0)) as total'));
+
+        if ($level === 'Pemilik') {
+            $kinerjaCabang = $kinerjaCabang->whereIn(DB::raw('json_extract(cabang, \'$.id_cabang\')'), function (Builder $builder) use ($idPengguna) {
+                $builder = $builder->select('id_cabang');
+                $builder = $builder->from('cabang_pemilik');
+
+                $builder->where('id_pengguna', '=', $idPengguna);
+            });
+        }
 
         if ($cabang) {
             $kinerjaCabang = $kinerjaCabang->whereRaw('json_unquote(json_extract(cabang, \'$.nama_cabang\')) like ?', [sprintf('%%%s%%', $cabang)]);
